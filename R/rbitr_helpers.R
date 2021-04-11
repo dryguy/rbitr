bb5aec88333b1367dc404380c2d80c2638a0a46f
dryguy <dryguy@comcast.net>
2021-04-11 17:52:25 -0400

    added nested_lapply

---
+++ R/rbitr_helpers.R
@@ -0,0 +1,15 @@
+#' Apply `input_function` to the root level elements of a nested list.
+#'
+#' @param x A nested list.
+#' @param input_function A function that takes the root level elements of x as
+#     its arguments.
+#' @param ... Further arguments to `input_function`.
+#'
+#' @return A list with the same structure as x, where the root level elements
+#'   have been replaced with the outputs of `input_function`.
+nested_lapply <- function (x, input_function, ...) {
+  lapply_input_function <- function (x, ...) {
+    lapply(x, input_function, ...)
+  }
+  lapply(x, lapply_input_function, ...)
+}
---
+++ tests/testthat/test-rbitr_helpers.R
@@ -0,0 +1,25 @@
+list_1 <- list(
+  list(list(1, 2, 3), list(4, 5, 6)),
+  list(list(7, 8, 9), list(10, 11, 12))
+)
+list_2 <- list(
+  list(c(1, 2, 3), c(4, 5, 6)),
+  list(c(7, 8, 9), c(10, 11, 12))
+)
+list_3 <- list(
+  list(list(1, 2, 3), list(4, 5, 6)),
+  list(list(7, 8, 9), numeric(0))
+)
+list_4 <- list(
+  list(list(1, 2, 3), list(4, 5, 6)),
+  list(list(7, 8, 9), NA)
+)
+test_that ('nested_lapply applies input_function to root levels of x', {
+  test <- nested_lapply(list_1, unlist)
+  expect_identical(test, list_2)
+  replace_empty_vectors_with_NA <- function (y) {
+    if (length(y) == 0) {NA} else {y}
+  }
+  test_2 <- nested_lapply(list_3, replace_empty_vectors_with_NA)
+  expect_identical(test_2, list_4)
+})
