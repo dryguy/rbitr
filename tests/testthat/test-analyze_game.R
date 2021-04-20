a0625bc07672cbfa4d52fd883b4a0a0521f9ea0c
dryguy <dryguy@comcast.net>
2021-04-20 18:45:13 -0400

    added analyze_game

---
+++ tests/testthat/test-analyze_game.R
@@ -0,0 +1,9 @@
+test_that('analyze_game analyzes a game', {
+  movetext <- '1. e4 g5 2. Nc3 f5 3. Qh5# 1-0'
+  engine <- bigchess::uci_engine('//stockfish_13_win_x64_bmi2.exe')
+  analysis <- analyze_game(movetext, engine, n_pv = 1, depth = 1)
+  expect_identical(class(analysis), 'list')
+  expect_identical(analysis[[1]][2], 'readyok')
+  expect_identical(analysis[[6]][5], 'bestmove (none)')
+  bigchess::uci_quit(engine)
+})
