ed60d996371bcc6591274841ffe778669dc9c8a1
dryguy <dryguy@comcast.net>
2021-05-07 20:52:29 -0400

    added lichess_plot() with time only

--- R/lichess_plot.R
+++ R/lichess_plot.R
@@ -1,3 +1,69 @@
-lichess_plot <- function() {
+lichess_plot <- function(white_move_times, black_move_times, scores) {
+  assertthat::assert_that(is.integer(white_move_times) |
+                          is.numeric(white_move_times))
+  assertthat::assert_that(is.integer(black_move_times) |
+                          is.numeric(black_move_times))
+  assertthat::assert_that(is.integer(scores) | is.numeric(scores))
+# TODO validate # of moves
 
+  white_move_times <- c(0, white_move_times)
+  black_move_times <- c(0, black_move_times)
+  max_y <- max(c(white_move_times, black_move_times))
+  y_offset <- 0.01 * max_y
+  y_lim <- 1.4 * max_y
+  if (length(black_move_times) < length(white_move_times)) {
+    black_move_times <- c(black_move_times, NA)
+  }
+  n_ply <- length(white_move_times) + length(black_move_times)
+  ply <- 1:n_ply
+  lichess_move_times <- data.frame(
+    white_ply = ply[ply %% 2 == 1] + 0.5,
+    black_ply = ply[ply %% 2 == 0] + 0.5,
+    white_times = white_move_times,
+    black_times = black_move_times
+  )
+  # Set up background color gradient
+  sigma <- 0.2 * y_lim
+  y_grad <- seq(from = -y_lim, to = y_lim, length.out = 200)
+  x_grad <- seq(from =     (n_ply - 1) / 4 + 1,
+                to   = 3 * (n_ply - 1) / 4 + 1,
+                length.out = 2)
+  grad <- expand.grid(x_grad = x_grad, y_grad = y_grad)
+  grad$z_grad <- exp(-(grad$y_grad^2) / (2 * sigma^2))
+  # ---
+  y_grad2 <- seq(from = -max_y, to = 0, length.out = 100)
+  grad2 <- expand.grid(x_grad2 = x_grad, y_grad2 = y_grad2)
+  grad2$z_grad2 <- exp(-(grad2$y_grad2^2) * (2 * sigma^2))
+
+  # Make the move times plot
+  p_time <- ggplot2::ggplot() +
+    ggplot2::geom_tile(data = grad,
+              mapping = ggplot2::aes(x_grad, y_grad, fill = z_grad),
+              alpha = 0.8) +
+    ggplot2::scale_fill_gradient2(low = rgb(216, 214, 212, maxColorValue = 255),
+                                 mid  = rgb(237, 235, 233, maxColorValue = 255),
+                                 high = rgb(216, 214, 212, maxColorValue = 255)) +
+    ggplot2::geom_area(data = lichess_move_times,
+      mapping = ggplot2::aes(x = white_ply, y = white_times + y_offset),
+      color   = rgb(83,  160, 233, maxColorValue = 255),
+      fill    = rgb(252, 251, 250, maxColorValue = 255),
+      alpha   = 0.75) +
+    ggplot2::geom_point(data = lichess_move_times,
+      mapping = ggplot2::aes(x = white_ply, y = white_times + y_offset),
+      color   = rgb(83,  160, 233, maxColorValue = 255),
+      size = 1) +
+    ggplot2::geom_area(data = lichess_move_times,
+      mapping = ggplot2::aes(x = black_ply, y = -black_times - y_offset),
+      fill    = rgb(177, 175, 174, maxColorValue = 255),
+      color   = rgb(83,  160, 233, maxColorValue = 255),
+      na.rm   = TRUE) +
+    ggplot2::geom_point(data = lichess_move_times,
+      mapping = ggplot2::aes(x = black_ply, y = -black_times - y_offset),
+      color   = rgb(83,  160, 233, maxColorValue = 255),
+      size = 1,
+      na.rm   = TRUE) +
+    ggplot2::xlim(c(1, n_ply)) +
+    ggplot2::theme_void() +
+    ggplot2::theme(legend.position = 'none') +
+    ggplot2::coord_trans(x = 'identity', y = 'move_time')
 }
--- R/rbitr_helpers.R
+++ R/rbitr_helpers.R
@@ -13,3 +13,20 @@ nested_lapply <- function(x, input_function, ...) {
   }
   lapply(x, lapply_input_function, ...)
 }
+
+
+# Axis scaling
+scale_move_times <- function(move_times) {
+  # Use the same logarithmic scale as lichess
+  # https://github.com/ornicar/lila/blob/442da0c86a9d54c3cff5645e14d67dfe269a9d0b/public/javascripts/chart/movetime.js
+  # logC = Math.pow(Math.log(3), 2)
+  # y = Math.pow(Math.log(.005 * Math.min(time, 12e4) + 3), 2) - logC
+  move_times[move_times > 12e4] <- 12e4
+  log(0.005 * move_times + 3)^2 - log(3)^2
+  # move_times / max(move_times, na.rm = TRUE)
+}
+inv_scale_move_times <- function(scaled_times) {
+  200 * (exp(1)^(sqrt(scaled_times + (log(3))^2)) - 3)
+}
+move_time_trans <- function() scales::trans_new(
+  "move_time", scale_move_times, scale_move_times)
