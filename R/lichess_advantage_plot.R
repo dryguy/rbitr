7e3a1bf128e103b16aa5f999893ed9604bd19a75
dryguy <dryguy@comcast.net>
2021-05-11 19:07:20 -0400

    added documentation for lichess_advantage_plot

--- R/lichess_advantage_plot.R
+++ R/lichess_advantage_plot.R
@@ -1,11 +1,28 @@
+#' Generate an advantage plot similar to the ones in the "Computer analysis" tab
+#'   on lichess.org.
+#'
+#' @details Given a vector of positional evaluations (in centipawns), the
+#'   results are scaled using the same logarithmic function used by lichess.org,
+#'   and the results are plotted using the same color scheme as lichess.org.
+#'
+#' @param scores A numeric or integer vector of positional evaluations (in
+#'   centipawns.)
+#'
+#' @return A ggplot object of the plotted data.
+#' @export
+#'
+#' @examples
+#'   scores <- c(15, 5, 29, -94, 67, 76, 154, -31, 1000, 1000)
+#'   lichess_advantage_plot(scores)
 lichess_advantage_plot <- function(scores) {
   # Validate input
   assertthat::assert_that(is.numeric(scores) |
                           is.integer(scores))
 
-  #Make the 2 color area plot
+  # Make the 2 color area plot
+  n_ply <- length(scores)
   score_frame <- data.frame(
-    ply    = 1:length(scores),
+    ply    = 1:n_ply,
     scores = winning_chances(scores)
   )
   plot_2_color_area(score_frame, 'ply', 'scores') +
@@ -13,7 +30,7 @@ lichess_advantage_plot <- function(scores) {
     ggplot2::geom_point(data    = data.frame(x = c(1, 1), y = c(-1, 1)),
                         mapping = ggplot2::aes(x = .data$x, y = .data$y),
                         alpha   = 0) +
-    ggplot2::geom_segment(ggplot2::aes(x = 1, y = 0, xend = .data$n_ply, yend = 0),
+    ggplot2::geom_segment(ggplot2::aes(x = 1, y = 0, xend = n_ply, yend = 0),
                           color = grDevices::rgb(160, 160, 160,
                                                  maxColorValue = 255)) +
     ggplot2::geom_line(data    = score_frame,
