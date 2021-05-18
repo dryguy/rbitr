81066092dae86e26eb99d7dfb2391f06f6db33d4
dryguy <dryguy@comcast.net>
2021-05-18 16:53:42 -0400

    added function get_moves()

---
+++ R/get_moves.R
@@ -0,0 +1,20 @@
+#' Gets LAN format moves from a pgn movetext vector
+#'
+#' Given a character vector of movetext (typically obtained from a pgn using
+#'   rbitr's `get_pgn()` function), return a list of character vectors of LAN
+#'   format moves.
+#'
+#' @param movetext A character vector of one or more movetext fields from a pgn.
+#'
+#' @return A list of character vectors, where each vector contains the LAN
+#'   format moves of the corresponding movetext.
+#' @export
+#'
+#' @examples
+#' movetext <- c('1. e4 e5', '1. h3 h6')
+#' get_moves(movetext)
+get_moves <- function(movetext) {
+  moves <- lapply(movetext, bigchess::san2lan)
+  moves <- unlist(lapply(moves, tolower), use.names = FALSE)
+  strsplit(moves, ' ', fixed = TRUE)
+}
---
+++ tests/testthat/test-get_moves.R
@@ -0,0 +1,6 @@
+test_that('get_moves gets moves', {
+  expect_identical(
+    get_moves(c('1. e4 e5', '1. h3 h6')),
+    list(c('e2e4', 'e7e5'), c('h2h3', 'h7h6'))
+  )
+})
