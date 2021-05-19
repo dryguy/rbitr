ae5d64c6e611166077020505d91c2500cb5e0c5e
dryguy <dryguy@comcast.net>
2021-05-18 21:10:49 -0400

    fixed problem with evaluate_game() and evaluate_pgn() creating extraneous gamelog lines after interrupted anayses

--- tests/testthat/test-evaluate_pgn.R
+++ tests/testthat/test-evaluate_pgn.R
@@ -4,11 +4,10 @@ pgn_path <- file.path(
   'dr_who.pgn'
 )
 test_that('evaluate_pgn evaluates a pgn', {
-  engine <- bigchess::uci_engine('//stockfish_13_win_x64_bmi2.exe')
-  evaluations <- evaluate_pgn(pgn_path, engine, n_pv = 2, go_mode = 'depth',
+  engine_path <- '//stockfish_13_win_x64_bmi2.exe'
+  evaluations <- evaluate_pgn(pgn_path, engine_path, n_pv = 2, go_mode = 'depth',
                               go_value = 2, mute = TRUE)
   expect_identical(class(evaluations), 'list')
   expect_identical(evaluations[[3]][[62]][2], 'readyok')
   expect_identical(evaluations[[2]][[78]][5], 'bestmove (none)')
-  bigchess::uci_quit(engine)
 })
