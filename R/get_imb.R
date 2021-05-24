bde1fd99c8b17da28a39a0cc3d8d6b0f91a1e985
dryguy <dryguy@comcast.net>
2021-05-23 20:01:42 -0400

    minor update to get_imb() example and test

--- R/get_imb.R
+++ R/get_imb.R
@@ -47,7 +47,7 @@
 #' @export
 #'
 #' @examples
-#' scores <- c(12, 171, -72, 50000, 50000)
+#' scores <- c(12, -171, -72, -50000, -50000)
 #' moves <- c("g2g4", "e7e6", "f2f4", "d8h4")
 #' bestmoves <- c("d2d4", "d7d5", "g1f3", "d8h4")
 #' get_imb(scores, moves, bestmoves, color = 'white')
--- tests/testthat/test-get_imb.R
+++ tests/testthat/test-get_imb.R
@@ -1,7 +1,7 @@
-scores <- c(12, 171, -72, 50000, 50000)
+scores <- c(12, -171, -72, -50000, -50000)
 moves <- c("g2g4", "e7e6", "f2f4", "d8h4")
 bestmoves <- c("d2d4", "d7d5", "g1f3", "d8h4")
 imb <- get_imb(scores, moves, bestmoves, color = 'white')
 test_that('get_imb gets inaccuracies, mistakes, and blunders', {
-  expect_equal(imb, list(inaccuracies = 0, mistakes = 0, blunders = 0))
+  expect_equal(imb, list(inaccuracies = 0, mistakes = 0, blunders = 2))
 })
