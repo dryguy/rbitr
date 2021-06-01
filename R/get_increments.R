734603700d5f143cc7494b0a82eede21398f1521
dryguy <dryguy@comcast.net>
2021-06-01 16:19:14 -0400

    polished get_increments() docs

--- R/get_increments.R
+++ R/get_increments.R
@@ -1,13 +1,13 @@
 #' Get increments from pgn TimeControl tags
 #'
-#' Extract the increment from a pgn time control tag. For allowed time control
-#'   formats, see the
+#' Extract the increments from a pgn file's time control tags. For allowed time
+#'   control formats, see the
 #'   [pgn specification](http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm#c9.6)
 #'   .
 #'
-#' @details The `get_increments()` function is intended for use with data read
-#'   from a pgn file using the `get_pgn()` function. If a TimeControl tag was
-#'   present in the pgn, a tibble obtained using `get_pgn()` will have a
+#' @details The `get_increments()` function is intended for use with data that
+#'   was read from a pgn file with the `get_pgn()` function. If a TimeControl
+#'   tag was present in the pgn, a tibble obtained using `get_pgn()` will have a
 #'   $TimeControl column from which the increment data can be extracted. Note
 #'   that there are six different time control formats recognized by the pgn
 #'   specification, only one of which uses increments. All other formats will
@@ -18,6 +18,15 @@
 #' @return An integer vector of the increments, in seconds. Time controls with
 #'   no increment return 0.
 #' @export
+#'
+#' @seealso
+#'   * [rbitr::get_pgn()] to load the time control and other data from a pgn
+#'     file.
+#'   * [rbitr::get_clocks()] to extract the clock data from a pgn file.
+#'   * [rbitr::get_move_times()] to calculate move times from clock and
+#'     increment data.
+#'   * [rbitr::lichess_time_plot()] and [rbitr::lichess_plot()] to plot move
+#'     times.
 #'
 #' @examples
 #' time_control <- c('?', '-', '40/9000', '300', '4500+60', '*180')
