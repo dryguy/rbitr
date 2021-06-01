553e0a0e3ea5a7815dcb59b9d6afdd651a509d38
dryguy <dryguy@comcast.net>
2021-06-01 18:36:46 -0400

    polished get_moves() docs

--- R/get_moves.R
+++ R/get_moves.R
@@ -1,17 +1,27 @@
-#' Gets LAN format moves from a pgn movetext vector
+#' Get the chess moves from a character vector containing movetext
 #'
-#' Given a character vector of movetext (typically obtained from a pgn using
-#'   rbitr's `get_pgn()` function), return a list of character vectors of LAN
-#'   format moves.
+#' Given a character vector of movetext, return a list of character vectors
+#'   each containing moves in long algebraic notation (LAN).
 #'
-#' @param movetext A character vector of one or more movetext fields from a pgn.
+#' @details The movetext field of a pgn record contains chess moves in standard
+#'   algebraic notation, along with optional annotations and comments. The
+#'   `get_moves()` function extracts the individual moves and converts them to
+#'   long algebraic notation (LAN). The movetext vectors are typically obtained
+#'   from a pgn file using the `get_pgn()` function.
+#'
+#' @param movetext A character vector of movetext.
 #'
 #' @return A list of character vectors, where each vector contains the LAN
 #'   format moves of the corresponding movetext.
 #' @export
 #'
+#' @seealso
+#'   * [rbitr::get_pgn()] to get the movetext and other data from a pgn file.
+#'   * [rbitr::clean_movetext()] to remove comments and annotations from
+#'     movetext.
+#'
 #' @examples
-#' movetext <- c('1. e4 e5', '1. h3 h6')
+#' movetext <- c('1. e4 {Best by test.} e5', '1. h3?! h6')
 #' get_moves(movetext)
 
 get_moves <- function(movetext) {
