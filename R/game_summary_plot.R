30396840e3aa557ad69814a6aba109b13c4e0bb8
Josh R <joshrof@gmail.com>
2023-05-06 18:17:30 -0400

    Added Functionality to game_summary_plot
    Added the ability to change the way that the end-user utilizes the engine. Added the ability to choose from nodes, depth and move times.

--- R/game_summary_plot.R
+++ R/game_summary_plot.R
@@ -48,175 +48,178 @@
 #' # Modify engine_path as required for your engine location & operating system
 #' engine_path <- '//stockfish.exe'
 #' game_summary_plot(pgn_path, game_number = 1, engine_path)
-game_summary_plot <- function(pgn_path, game_number, engine_path = NULL,
-                              n_cpus = 1, use_pgn_evals = TRUE, nodes = 2250000,
-                              style = 'graph') {
-  # Validate input
-  assertthat::assert_that(assertthat::is.string(pgn_path))
-  assertthat::assert_that(assertthat::is.count(game_number))
-  assertthat::assert_that(assertthat::is.string(engine_path) |
-                          is.null(engine_path))
-  assertthat::assert_that(assertthat::is.count(n_cpus))
-  assertthat::assert_that(assertthat::is.flag(use_pgn_evals))
-  assertthat::assert_that(style == 'graph' |
-                          style == 'infographic')
-  assertthat::assert_that(assertthat::is.count(nodes))
-  # Load the game
-  pgn <- get_pgn(pgn_path)
-  assertthat::assert_that(game_number <= nrow(pgn))
-  # Get the moves
-  moves <- get_moves(pgn$Movetext[[game_number]])
-  # Load the clocks
-  clocks <- get_clocks(pgn$Movetext[[game_number]])[[1]]
-  if ('TimeControl' %in% names(pgn)) {
-    increment <- get_increments(pgn$TimeControl[[game_number]])
-  } else {
-    increment <- 0
-  }
-  white_move_times <- get_move_times(clocks, increment, 'white')
-  black_move_times <- get_move_times(clocks, increment, 'black')
-  # Get evals
-    # Look in loaded pgn first
-  evals <- get_evals(pgn$Movetext[[game_number]])[[1]]
-  if (!identical(evals, numeric(0))) {
-    evals <- c(15, evals)
-  }
-    # Then look for analysis log; analyze game if log is missing
-  progress_path <- tools::file_path_sans_ext(pgn_path)
-  pgn_basename <- basename(pgn_path)
-  pgn_basename <- tools::file_path_sans_ext(pgn_basename)
-  save_path <- file.path(
-    progress_path,
-    paste0(pgn_basename, '_', 'nodes', '2250000', 'pv', '1', '_', game_number,
-           '.Rdata')
-  )
-  if (file.exists(save_path)) {
-    gamelog <- list(0)
-    load(save_path)
-    evaluation <- gamelog
-    rm(gamelog)
-  } else {
-    if (is.null(engine_path)) {
-      stop(paste0('No saved analysis exists and engine_path = NULL:/n',
-                  '* An engine is required to analyze the game.'))
+function(pgn_path, game_number, engine_path = NULL,
+         n_cpus = 1, use_pgn_evals = TRUE, limiter, limit,
+         style = 'graph') {
+    # Validate input
+    assertthat::assert_that(assertthat::is.string(pgn_path))
+    assertthat::assert_that(assertthat::is.count(game_number))
+    assertthat::assert_that(assertthat::is.string(engine_path) |
+                                is.null(engine_path))
+    assertthat::assert_that(assertthat::is.count(n_cpus))
+    assertthat::assert_that(assertthat::is.flag(use_pgn_evals))
+    assertthat::assert_that(limiter == 'depth' |
+                                limiter == 'nodes' |
+                                limiter == 'movetime')
+    assertthat::assert_that(assertthat::is.count(limit))
+    assertthat::assert_that(style == 'graph' |
+                                style == 'infographic')
+    # Load the game
+    pgn <- get_pgn(pgn_path)
+    assertthat::assert_that(game_number <= nrow(pgn))
+    # Get the moves
+    moves <- get_moves(pgn$Movetext[[game_number]])
+    # Load the clocks
+    clocks <- get_clocks(pgn$Movetext[[game_number]])[[1]]
+    if ('TimeControl' %in% names(pgn)) {
+        increment <- get_increments(pgn$TimeControl[[game_number]])
+    } else {
+        increment <- 0
     }
-    evaluation <- evaluate_game(pgn$Movetext[[game_number]], engine_path,
-                                n_pv = 1, limiter = 'nodes', limit = nodes)
-  }
-  if (!use_pgn_evals | identical(evals, numeric(0))) {
-    evals <- parse_gamelog(evaluation, target = 'score')
-    evals <- unlist(evals)
-    evals <- convert_scores(evals)
-  }
-  bestmoves <- parse_gamelog(evaluation, target = 'bestmove')
-  bestmoves <- unlist(bestmoves)
-  # Make the plots
-  ax <- 1 + 0.01 * (length(evals) - 1)
-  if (length(white_move_times) == 0) {
-    ty <- 0.95
-    tx <- -0.99
-  } else {
-    ty <- 0.95 * scale_move_times(max(c(0, white_move_times, black_move_times),
-                                      na.rm = TRUE))
-    tx <- ax
-  }
-  if (style == 'infographic') {
-    p_time_annotation <- ggplot2::annotate('text', x = tx, y = ty,
-                                           label = 'Scaled Move Times',
-                                           hjust = 0)
-    p_advantage_annotation <- ggplot2::annotate('text', x = ax, y = 0.95,
-                                                label = 'Scaled Advantage', hjust = 0)
-  } else if (style == 'graph') {
-    p_time_annotation <- ggplot2::geom_blank()
-    p_advantage_annotation <- ggplot2::geom_blank()
-  }
-  p1 <- time_plot(white_move_times, black_move_times, style = style,
-                  scaling = 'lichess') +
-    p_time_annotation
-  p2 <- advantage_plot(evals, style = style, scaling = 'lichess') +
-    p_advantage_annotation
-  moves <- get_moves(pgn$Movetext[[game_number]])[[1]]
-  white_imb <- get_imb(evals, moves, bestmoves, 'white')
-  black_imb <- get_imb(evals, moves, bestmoves, 'black')
-  white_acpl <- get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
-  black_acpl <- get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
-  white_table <- data.frame(
-    c('\u25CB',
-      length(white_imb$inaccuracies),
-      length(white_imb$mistakes),
-      length(white_imb$blunders),
-      get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
-    ),
-    c(pgn$White[[game_number]],
-      'inaccuracies',
-      'mistakes',
-      'blunders',
-      'Average cewntipawn loss'
+    white_move_times <- get_move_times(clocks, increment, 'white')
+    black_move_times <- get_move_times(clocks, increment, 'black')
+    # Get evals
+    # Look in loaded pgn first
+    evals <- get_evals(pgn$Movetext[[game_number]])[[1]]
+    if (!identical(evals, numeric(0))) {
+        evals <- c(15, evals)
+    }
+    # Then look for analysis log; analyze game if log is missing
+    progress_path <- tools::file_path_sans_ext(pgn_path)
+    pgn_basename <- basename(pgn_path)
+    pgn_basename <- tools::file_path_sans_ext(pgn_basename)
+    save_path <- file.path(
+        progress_path,
+        paste0(pgn_basename, '_', limiter, limit, 'pv', '1', '_', game_number,
+               '.Rdata')
     )
-  )
-  black_table <- data.frame(
-    c('\u25CF',
-      length(black_imb$inaccuracies),
-      length(black_imb$mistakes),
-      length(black_imb$blunders),
-      get_acpl(evals, 'black', cap = 1000, cap_action = 'replace')
-    ),
-    c(pgn$Black[[game_number]],
-      'inaccuracies',
-      'mistakes',
-      'blunders',
-      'Average centipawn loss'
+    if (file.exists(save_path)) {
+        gamelog <- list(0)
+        load(save_path)
+        evaluation <- gamelog
+        rm(gamelog)
+    } else {
+        if (is.null(engine_path)) {
+            stop(paste0('No saved analysis exists and engine_path = NULL:/n',
+                        '* An engine is required to analyze the game.'))
+        }
+        evaluation <- evaluate_game(pgn$Movetext[[game_number]], engine_path,
+                                    n_pv = 1, limiter = limiter, limit = limit)
+    }
+    if (!use_pgn_evals | identical(evals, numeric(0))) {
+        evals <- parse_gamelog(evaluation, target = 'score')
+        evals <- unlist(evals)
+        evals <- convert_scores(evals)
+    }
+    bestmoves <- parse_gamelog(evaluation, target = 'bestmove')
+    bestmoves <- unlist(bestmoves)
+    # Make the plots
+    ax <- 1 + 0.01 * (length(evals) - 1)
+    if (length(white_move_times) == 0) {
+        ty <- 0.95
+        tx <- -0.99
+    } else {
+        ty <- 0.95 * scale_move_times(max(c(0, white_move_times, black_move_times),
+                                          na.rm = TRUE))
+        tx <- ax
+    }
+    if (style == 'infographic') {
+        p_time_annotation <- ggplot2::annotate('text', x = tx, y = ty,
+                                               label = 'Scaled Move Times',
+                                               hjust = 0)
+        p_advantage_annotation <- ggplot2::annotate('text', x = ax, y = 0.95,
+                                                    label = 'Scaled Advantage', hjust = 0)
+    } else if (style == 'graph') {
+        p_time_annotation <- ggplot2::geom_blank()
+        p_advantage_annotation <- ggplot2::geom_blank()
+    }
+    p1 <- time_plot(white_move_times, black_move_times, style = style,
+                    scaling = 'lichess') +
+        p_time_annotation
+    p2 <- advantage_plot(evals, style = style, scaling = 'lichess') +
+        p_advantage_annotation
+    moves <- get_moves(pgn$Movetext[[game_number]])[[1]]
+    white_imb <- get_imb(evals, moves, bestmoves, 'white')
+    black_imb <- get_imb(evals, moves, bestmoves, 'black')
+    white_acpl <- get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
+    black_acpl <- get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
+    white_table <- data.frame(
+        c('\u25CB',
+          length(white_imb$inaccuracies),
+          length(white_imb$mistakes),
+          length(white_imb$blunders),
+          get_acpl(evals, 'white', cap = 1000, cap_action = 'replace')
+        ),
+        c(pgn$White[[game_number]],
+          'inaccuracies',
+          'mistakes',
+          'blunders',
+          'Average cewntipawn loss'
+        )
     )
-  )
-  white_col_a <- c('\u25CB',
-                   length(white_imb$inaccuracies),
-                   length(white_imb$mistakes),
-                   length(white_imb$blunders),
-                   get_acpl(evals, 'white', cap = 1000, cap_action = 'replace'))
-  white_col_b <- c(pgn$White[[game_number]],
-                   'inaccuracies', 'mistakes', 'blunders',
-                   'Average cewntipawn loss')
-  white_caption <- make_table(white_col_a, white_col_b)
-  black_col_a <- c('\u25CF',
-                   length(black_imb$inaccuracies),
-                   length(black_imb$mistakes),
-                   length(black_imb$blunders),
-                   get_acpl(evals, 'black', cap = 1000, cap_action = 'replace'))
-  black_col_b <- c(pgn$Black[[game_number]],
-                   'inaccuracies', 'mistakes', 'blunders',
-                   'Average cewntipawn loss')
-  black_caption <- make_table(black_col_a, black_col_b)
-
-  # Generate stat tables
-   # White
-  tw <- ggplot2::ggplot(data = data.frame(x = 0, y = 0),
-                        ggplot2::aes(x = .data$x, y = .data$y)) +
-    ggfittext::geom_fit_text(
-      label = white_caption,
-      family = 'mono',
-      min.size = 0,
-      hjust = 0
-    ) +
-    ggplot2::theme_void() +
-    ggplot2::theme(plot.background = ggplot2::element_rect(
-      fill  = grDevices::rgb(237, 235, 233, maxColorValue = 255),
-      color = grDevices::rgb(237, 235, 233, maxColorValue = 255)))
-  tb <- ggplot2::ggplot(data = data.frame(x = 0, y = 0),
-                        ggplot2::aes(x = .data$x, y = .data$y)) +
-    ggfittext::geom_fit_text(
-      label = black_caption,
-      family = 'mono',
-      min.size = 0,
-      hjust = 0
-    ) +
-    ggplot2::theme_void() +
-    ggplot2::theme(plot.background = ggplot2::element_rect(
-      fill  = grDevices::rgb(237, 235, 233, maxColorValue = 255),
-      color = grDevices::rgb(237, 235, 233, maxColorValue = 255)))
-# Output the result
-  patchwork::wrap_plots(
-    list(p1, tw, p2, tb),
-    design = 'AAAAAABB
+    black_table <- data.frame(
+        c('\u25CF',
+          length(black_imb$inaccuracies),
+          length(black_imb$mistakes),
+          length(black_imb$blunders),
+          get_acpl(evals, 'black', cap = 1000, cap_action = 'replace')
+        ),
+        c(pgn$Black[[game_number]],
+          'inaccuracies',
+          'mistakes',
+          'blunders',
+          'Average centipawn loss'
+        )
+    )
+    white_col_a <- c('\u25CB',
+                     length(white_imb$inaccuracies),
+                     length(white_imb$mistakes),
+                     length(white_imb$blunders),
+                     get_acpl(evals, 'white', cap = 1000, cap_action = 'replace'))
+    white_col_b <- c(pgn$White[[game_number]],
+                     'inaccuracies', 'mistakes', 'blunders',
+                     'Average cewntipawn loss')
+    white_caption <- make_table(white_col_a, white_col_b)
+    black_col_a <- c('\u25CF',
+                     length(black_imb$inaccuracies),
+                     length(black_imb$mistakes),
+                     length(black_imb$blunders),
+                     get_acpl(evals, 'black', cap = 1000, cap_action = 'replace'))
+    black_col_b <- c(pgn$Black[[game_number]],
+                     'inaccuracies', 'mistakes', 'blunders',
+                     'Average cewntipawn loss')
+    black_caption <- make_table(black_col_a, black_col_b)
+    
+    # Generate stat tables
+    # White
+    tw <- ggplot2::ggplot(data = data.frame(x = 0, y = 0),
+                          ggplot2::aes(x = .data$x, y = .data$y)) +
+        ggfittext::geom_fit_text(
+            label = white_caption,
+            family = 'mono',
+            min.size = 0,
+            hjust = 0
+        ) +
+        ggplot2::theme_void() +
+        ggplot2::theme(plot.background = ggplot2::element_rect(
+            fill  = grDevices::rgb(237, 235, 233, maxColorValue = 255),
+            color = grDevices::rgb(237, 235, 233, maxColorValue = 255)))
+    tb <- ggplot2::ggplot(data = data.frame(x = 0, y = 0),
+                          ggplot2::aes(x = .data$x, y = .data$y)) +
+        ggfittext::geom_fit_text(
+            label = black_caption,
+            family = 'mono',
+            min.size = 0,
+            hjust = 0
+        ) +
+        ggplot2::theme_void() +
+        ggplot2::theme(plot.background = ggplot2::element_rect(
+            fill  = grDevices::rgb(237, 235, 233, maxColorValue = 255),
+            color = grDevices::rgb(237, 235, 233, maxColorValue = 255)))
+    # Output the result
+    patchwork::wrap_plots(
+        list(p1, tw, p2, tb),
+        design = 'AAAAAABB
               CCCCCCDD'
-  )
+    )
 }
