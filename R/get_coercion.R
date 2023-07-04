5e14f83742a031b441e04ffd937e1d2ce114feee
dryguy <dryguy@comcast.net>
2023-07-04 03:01:04 -0400

    Update docs from previous commit.

--- R/get_coercion.R
+++ R/get_coercion.R
@@ -43,7 +43,8 @@
 #'   "info depth 1 seldepth 1 multipv 1 score cp 161 nodes 59 nps 59000 tbhits 0 time 1 pv b1c3",
 #'   "info depth 1 seldepth 1 multipv 2 score cp 146 nodes 59 nps 59000 tbhits 0 time 1 pv d2d4",
 #'   "info depth 2 seldepth 2 multipv 1 score cp 195 nodes 147 nps 73500 tbhits 0 time 2 pv d2d4 g8f6",
-#'   "info depth 2 seldepth 2 multipv 2 score cp 170 nodes 147 nps 73500 tbhits 0 time 2 pv h2h4 g8f6 h4g5",
+#'   "info depth 2 seldepth 2 multipv 2 score cp 170 nodes 147 nps 73500 tbhits
+#'   0 time 2 pv h2h4 g8f6 h4g5",
 #'   "bestmove d2d4 ponder g8f6"
 #' )
 get_coercion <- function(positionlog) {
