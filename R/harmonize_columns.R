ab9ad96c5f494b69196c8c80c6fd280b5b87848a
dryguy <dryguy@comcast.net>
2023-07-11 16:56:09 -0400

    Changed harmonize_columns() to exported.

--- R/harmonize_columns.R
+++ R/harmonize_columns.R
@@ -2,11 +2,23 @@
 #'
 #' This function takes a list of data frames and adds missing columns to each
 #' data frame so that all data frames have the same columns in the same order.
-#' Any added columns will be populated with NAs. The function can be used to
-#' prepare data frames for an rbind when the columns are not identical.
+#'
+#' @details Any added columns will be populated with NAs. The function can be
+#' used to prepare data frames for an rbind when the columns are not identical.
 #'
 #' @param df_list A list of data frames
+#'
 #' @return A list of data frames with harmonized columns
+#' @export
+#'
+#' @examples
+#' # Create some example data frames
+#' df1 <- data.frame(a = 1:3, b = letters[1:3])
+#' df2 <- data.frame(b = letters[4:6], c = 4:6)
+#' df3 <- data.frame(a = 7:9, c = letters[7:9])
+#'
+#' # Harmonize columns
+#' harmonize_columns(list(df1, df2, df3))
 harmonize_columns <- function(df_list) {
   # Find all unique column names across all data frames
   all_colnames <- unique(unlist(lapply(df_list, colnames)))
