b7c50f1311faa9860e6080759031b8b432a1bf3c
dryguy <dryguy@comcast.net>
2023-11-08 13:43:31 -0500

    Revising the definition and documentation for count_ply() and adding count_moves() and count_positions().

--- R/count_ply.R
+++ R/count_ply.R
@@ -1,13 +1,30 @@
-#' Count the number of ply or halfmoves in a chess game
+#' Count the moves, half-moves, or positions in a chess game
 #'
-#' The functions `count_ply()` and `count_halfmoves()` take the movetext of a
-#' chess game as input and return either the number of ply (positions) or the
-#' number of half-moves in the game.
+#' * `count_moves()` counts the number of moves in the game.
+#' * `count_halfmoves()` and `count_ply()` count the number of half-moves in the game.
+#' * `count_positions()` and `count_nodes()` count the number of positions in the game.
 #'
-#' @rdname counts
+#' @details In chess, the following terms are used to describe the progression
+#'   of the game:
 #'
-#' @details Repeated positions or repeated half moves are included in the count.
-#'   The number of half-moves is always one less than the number of ply.
+#'   Move: A move in chess refers to a sequence in which each player has played
+#'   one turn. So, one move consists of white playing a "half-move" followed by
+#'   black playing another "half-move." Thus the number of moves can be an
+#'   integer or half-integer value. The number of moves is given by the function
+#'   `count_moves()`. Repeated moves are included in the count.
+#'
+#'   Half-Move or Ply: A half-move is a single turn taken by one player. The
+#'   number of half-moves is given by the function `count_halfmoves()` or its
+#'   alias `count_ply()`. Repeated half-moves are included in the count.
+#'
+#'   Position or Node: A chess position is the arrangement of the pieces on the
+#'   board along with the information about castling rights, *en passant*
+#'   captures, the side to move, and the 50 move rule. Half-moves are
+#'   transitions from one position to the next, thus the number of positions is
+#'   always one more than the number of half-moves. The number of positions in a
+#'   game is given by the function `count_positions()` or its alias
+#'   `count_nodes()`. The starting position is always included in the count.
+#'   Repeated positions are also included in the count.
 #'
 #' @param movetext A character string containing the movetext of a chess game in
 #'   either standard algebraic notation (SAN) or
@@ -15,19 +32,43 @@
 #'   algebraic notation (LAN).
 #'
 #' @return
-#' * `count_ply()` returns an integer of the number of ply in the game.
-#' * `count_halfmoves()` returns an integer of the number of half-moves in the game.
+#' * `count_ply()` and `count_halfmoves()` return an integer of the number of half-moves in the game.
+#' * `count_nodes()` and `count_positions()` return an integer of the number of positions in the game.
+#' * `count_moves()` returns the number of moves in the game, which can take on integer or half-integer values.
 #'
 #' @export
 #'
+#' @rdname count_functions
+#'
 #' @examples
+#' count_moves("1. e4 e5 2. Nf3 Nc6")
 #' count_ply("1. e4 e5 2. Nf3 Nc6")
 #' count_halfmoves("1. e4 e5 2. Nf3 Nc6")
+#' count_positions("1. e4 e5 2. Nf3 Nc6")
+#' count_nodes("1. e4 e5 2. Nf3 Nc6")
 count_ply <- function(movetext) {
-  return(count_halfmoves(movetext) + 1L)
+  return(count_halfmoves(movetext))
 }
 
-#' @rdname counts
+#' @rdname count_functions
+#' @export
+count_nodes <- function(movetext) {
+  return(count_ply(movetext) + 1L)
+}
+
+#' @rdname count_functions
+#' @export
+count_positions <- function(movetext) {
+  return(count_ply(movetext) + 1L)
+}
+
+#' @rdname count_functions
+#' @export
+count_moves <- function(movetext) {
+  return(count_halfmoves(movetext) / 2)
+}
+
+#' @rdname count_functions
 #' @export
 count_halfmoves <- function(movetext) {
   # Validate input
--- tests/testthat/test-count_ply.R
+++ tests/testthat/test-count_ply.R
@@ -1,18 +1,28 @@
-test_that("count_ply works correctly", {
-  expect_equal(count_ply("1. e4 e5 2. Nf3 Nc6"), 5)
-  expect_equal(count_ply("1. e4 e5"), 3)
-  expect_equal(count_ply("e2e4 e7e5"), 3)
+test_that("count_moves works correctly", {
+  expect_equal(count_moves("1. e4 e5 2. Nf3 Nc6"), 2)
+  expect_equal(count_moves("1. e4 e5 2. Nf3"), 1.5)
+  expect_equal(count_moves("e2e4 e7e5"), 1)
 })
 
-test_that("count_halfmoves works correctly", {
-  expect_equal(count_halfmoves("1. e4 e5 2. Nf3 Nc6"), 4)
-  expect_equal(count_halfmoves("1. e4 e5"), 2)
+test_that("count_ply works correctly", {
+  expect_equal(count_ply("1. e4 e5 2. Nf3 Nc6"), 4)
+  expect_equal(count_ply("1. e4 e5"), 2)
+  expect_equal(count_ply("e2e4 e7e5"), 2)
   expect_equal(count_halfmoves("e2e4 e7e5"), 2)
+})
+
+test_that("count_positions works correctly", {
+  expect_equal(count_positions("1. e4 e5 2. Nf3 Nc6"), 5)
+  expect_equal(count_positions("1. e4 e5"), 3)
+  expect_equal(count_positions("e2e4 e7e5"), 3)
+  expect_equal(count_nodes("e2e4 e7e5"), 3)
 })
 
 test_that("count_ply and count_halfmoves validate input", {
   expect_error(count_ply(1))
   expect_error(count_halfmoves(1))
-  expect_error(count_ply(c("1. e4 e5 2. Nf3 Nc6", "1. e4 e5 2. Nf3 Nc6")))
-  expect_error(count_halfmoves(c("1. e4 e5 2. Nf3 Nc6", "1. e4 e5 2. Nf3 Nc6")))
+  expect_error(count_moves(1))
+  expect_error(count_ply(c("1. e4 e5", "1. d4 d5")))
+  expect_error(count_halfmoves(c("1. e4 e5", "1. d4 d5")))
+  expect_error(count_moves(c("1. e4 e5", "1. d4 d5")))
 })
